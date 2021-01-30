f3ab6f0eb07b11f3eb0b886a79b7bbe92c8dc2c2
Krenar123 <krenardardhishta205@gmail.com>
2021-01-30 20:56:19 +0100

    Notification table created

---
+++ database/migrations/2021_01_30_194900_create_table_notifications.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableNotifications extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id();
+            $table->integer('market_id');
+            $table->string('product_name');
+            $table->string('address');
+            $table->string('quantity');
+            $table->string('price');
+            $table->string('shipping_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications');
+    }
+}
