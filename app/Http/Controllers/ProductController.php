6bad8a99c758d9dfb600572e7e8d793e60954d63
Krenar123 <krenardardhishta205@gmail.com>
2021-03-23 15:45:22 +0100

    changes added

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,10 +12,7 @@ use Auth;
 
 class ProductController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+    
     /**
      * Display a listing of the resource.
      *
